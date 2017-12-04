ead8046bf238bb48799ca2a1ca58a5f5acf20fce
sujit sah <sujitsah60@gmail.com>
2017-12-05 01:02:24 +0530

    inital recipe list table added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'hellllooo', 'https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg')
+  ];
 
   constructor() { }
 
